b76f2beaaad1a202e8b9c481a1681ab51d100ff2
{airin grigorian} <agrigorian33@gmail.com>
2024-05-13 16:14:39 -0700

    revised question, function in index

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const {Triangle, Circle, Square} = require('./lib/shapes')
+const {Triangle, Circle, Square} = require('./lib/shapes');
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { userInfo } = require('os');
+
+//function for user input with requirements requesting it contains min 1 character and a max 3 characters
+
+function getUserInput() {
+    let userInput;
+    do {
+        userInput = prompt('Please choose a value with 1 to 3 characters:');
+    } while (!userInput || userInput.length < 1 || userInput.length > 3);
+    return userInput;
+}
 
 //user questions
-
 const questions = [ 
 {
     type: 'input',
@@ -18,12 +28,12 @@ const questions = [
 {
     type: 'list',
     name: 'shape',
-    prompt: 'Please choose a shape',
+    prompt: 'Please choose a shape from these available options',
     options: ['Trianle', 'Circle', 'Square'],
 },
 {
     type: 'input',
     name: 'shapeColor',
-    prompt: 'What color would you like the shape to be?',
+    prompt: 'What color would you like the logo shape to be?',
 },
 ];
